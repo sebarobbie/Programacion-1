bf89b5a14618c20f33d4f51abaaf3c8101cdf951
Sebastian Francisco <76788336+sebarobbie@users.noreply.github.com>
2024-01-24 18:09:08 -0400

    Add files via upload

---
+++ Año Bisiesto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un año: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if (EsBisiesto(year))
+        {
+            Console.WriteLine("Es bisiesto");
+        }
+        else
+        {
+            Console.WriteLine("No es bisiesto.");
+        }
+    }
+
+    static bool EsBisiesto(int year)
+    {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
+}
